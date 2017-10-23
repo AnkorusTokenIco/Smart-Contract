f5aee48e34e75b75fb4f15f845b3abcf683dbaab
Inzidious <voshtt@gmail.com>
2017-10-23 12:38:48 +0700

    updated smart contract
    added whitelist, lockout and transfer protocols

--- AnkorusToken.sol
+++ AnkorusToken.sol
@@ -20,6 +20,12 @@ contract AnkorusTestToken is BasicToken, Ownable
     // Amount of raised money in wei.
     uint256 public weiRaised;
     
+    //  Whitelist approval mapping
+    mapping (address => bool) public whitelist;
+    
+    //  Lockout mapping
+    mapping (address => uint256) public lockoutMap;
+    
     //  This is the 'Ticker' symbol and name for our Token.
     string public constant symbol = "ANKV10";
     string public constant name = "Ankorus Test Token V10";
@@ -30,7 +36,7 @@ contract AnkorusTestToken is BasicToken, Ownable
     // Event
     event TokenPurchase(address indexed purchaser, uint256 value, 
         uint256 amount);
-    event PreICOTokenPushed(address indexed buyer, uint256 amount);
+    event CompanyTokenPushed(address indexed buyer, uint256 amount);
     
     function AnkorusTestToken()
     {
@@ -92,47 +98,89 @@ contract AnkorusTestToken is BasicToken, Ownable
         balanceOf[0xb1] = saleCap;
     }
     
-    // fallback function can be used to buy tokens
+    //  Fallback function is entry point to buy tokens
     function () payable
     {
         buyTokens(msg.sender, msg.value);
     }
 
-    function saleActive() public constant returns (bool) 
-    {
-        return (getCurrentTimestamp() >= startDate &&
-                getCurrentTimestamp() < endDate && supply() > 0);
-    }
-
-    // low level token purchase function
+    //  Internal token purchase function
     function buyTokens(address beneficiary, uint256 value) payable
     {
-        /*require(beneficiary != 0x0);
-        
+        require(beneficiary != 0x0);
         require(value >= 0.1 ether);
-    
         
-        uint256 updatedWeiRaised = weiRaised.add(weiAmount);
-        weiRaised = updatedWeiRaised;*/
-    
         // Calculate token amount to be purchased
         uint256 weiAmount = value;
         uint256 actualRate = getRateAt(getCurrentTimestamp());
         uint256 amount = weiAmount.mul(actualRate);
 
         //  Check our supply
+        //  Potentially redundant as balanceOf[0xb1].sub(amount) will
+        //  throw with insufficient supply
         require(supply() >= amount);
 
-        //address owner = 0x4f04B097f04A2376bC380245008bFDF88D05B836;
+        //  Check conditions for sale
+        require(saleActive());
         
         // Transfer
-        require(saleActive());
-        //uint256 amount = 4000;
         balanceOf[0xb1] = balanceOf[0xb1].sub(amount);
         balanceOf[beneficiary] = balanceOf[beneficiary].add(amount);
-        //TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+        //TokenPurchase(msg.sender, beneficiary, weiAmount, amount);
 
-        //forwardFunds();
+        // Update state.
+        uint256 updatedWeiRaised = weiRaised.add(weiAmount);
+        weiRaised = updatedWeiRaised;
+
+        // Forward the fund to fund collection wallet.
+        wallet.transfer(msg.value);
+    }
+    
+    function addWhitelist(address beneficiary) onlyOwner
+    {
+        whitelist[beneficiary] = true;
+    }
+    
+    function transfer( address _recipient, uint256 _value ) returns( bool )
+    {
+        //  Check to see if the sender is locked out from transferring tokens
+        require(startDate + lockoutMap[msg.sender] < now );
+        
+        //  transfer
+        super.transfer( _recipient, _value );
+        
+        return true;
+    }
+    
+    function setLockout(address target, uint256 time) onlyOwner
+    {
+        lockoutMap[target] = time;
+    }
+    
+    function finalize() onlyOwner 
+    {
+        require(!saleActive());
+
+        // Transfer the remainder of tokens to Ankorus wallet
+        balanceOf[wallet] = balanceOf[wallet].add(balanceOf[0xb1]);
+        balanceOf[0xb1] = 0;
+    }
+
+    function saleActive() public constant returns (bool) 
+    {
+        //  Ability to purchase has begun for this purchaser with either 2 
+        //  conditions: Sale has started Or purchaser has been whitelisted to 
+        //  purchase tokens before The start date
+        bool checkSaleBegun = whitelist[msg.sender] || 
+            getCurrentTimestamp() >= startDate;
+        
+        //  Sale of tokens can not happen after the ico date or with no
+        //  supply in any case
+        bool canPurchase = checkSaleBegun && 
+            getCurrentTimestamp() < endDate &&
+            supply() > 0;
+            
+        return(canPurchase);
     }
     
     event Transfer( address indexed _owner, address indexed _recipient, uint256 _value );
